857156402fa877d795bf8d8722cab7434fd4d989
Denis Olekh <denisolekh@gmail.com>
2020-08-10 19:56:12 +0300

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
@@ -48,10 +47,7 @@ module.exports = (env, argv) => {
       })
     ],
     resolve: {
-      extensions: [".js", ".jsx"],
-      alias: {
-        images: path.resolve(__dirname, 'img')
-      }
+      extensions: [".js", ".jsx"]
     },
     devServer: {
       hot: true,
